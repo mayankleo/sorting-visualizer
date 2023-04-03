e44e58c696e3d8c513ef517dbdcf411234342f57
Mayank kushwaha <mayankkushwaha226@gmail.com>
2023-04-04 02:45:03 +0530

    made Dom object oriented

--- script.js
+++ script.js
@@ -6,36 +6,43 @@ const container = document.getElementsByClassName("container2");
 const range = document.getElementById("range");
 const sorts1 = document.getElementById("sorts1");
 const sorts2 = document.getElementById("sorts2");
-const s1c = document.getElementById("s1c");
-const s1s = document.getElementById("s1s");
-const s2c = document.getElementById("s2c");
-const s2s = document.getElementById("s2s");
 var obja, objb;
 
-function rangevalue(x){
+function rangevalue(x) {
   document.getElementById("rangev").innerHTML = x;
 }
 
 function generate() {
   obja = new Aswap("a");
   objb = new Aswap("b");
-  obja.checkScore.innerHTML=obja.swapScore.innerHTML=objb.checkScore.innerHTML=objb.swapScore.innerHTML=0;
+
+  obja.checkScore.innerHTML = obja.swapScore.innerHTML = objb.checkScore.innerHTML = objb.swapScore.innerHTML = 0;
   container[0].innerHTML = container[1].innerHTML = "";
-  el = range.value;
-  w = Math.floor((container[0].offsetWidth - (el - 1) * 4) / el);
+
+  let el = range.value;
+  let w = Math.floor((container[0].offsetWidth - (el - 1) * 4) / el);
+
   for (let i = 0; i < el; i++) {
-    ele = Math.floor(Math.random() * 50) + 10;
-    while (obja.arr.includes(ele)) {
-      ele = Math.floor(Math.random() * 50) + 10;
-    }
-    obja.arr[i] = objb.arr[i] = ele;
-    container[0].innerHTML += `<div class="pols" id="${obja.c}${ele}" style="height:${
-      obja.arr[i]}vh;width:${w}px;left:${w * i + 4 * i}px;">${obja.arr[i]}</div>`;
-    container[1].innerHTML += `<div class="pols" id="${objb.c}${ele}" style="height:${
-      objb.arr[i]}vh;width:${w}px;left:${w * i + 4 * i}px;">${objb.arr[i]}</div>`;
+    let rano = Math.floor(Math.random() * 50) + 10;
+    obja.arr[i] = new Polls(i, w, rano, 0);
+    objb.arr[i] = new Polls(i, w, rano, 1);
   }
-  let wcal = w * el + (el - 1) * 4 + "px";
-  container[0].style.width = container[1].style.width = wcal;
+  container[0].style.width = container[1].style.width = w * el + (el - 1) * 4 + "px";
+}
+
+class Polls {
+  constructor(i, w, rano, cno) {
+    this.no = rano;
+    this.xpos = w * i + 4 * i;
+    this.div = document.createElement("div");
+
+    this.div.classList.add("pols");
+    this.div.style.height = `${this.no}vh`;
+    this.div.style.width = `${w}px`;
+    this.div.style.left = `${this.xpos}px`;
+    this.div.textContent = this.no;
+    container[cno].append(this.div);
+  }
 }
 
 class Aswap {
@@ -45,41 +52,38 @@ class Aswap {
     this.checkScore = document.getElementById(`sc${this.c}`);
     this.swapScore = document.getElementById(`ss${this.c}`);
   }
-  async swap(r1, r2) {
-    let p1 = document.getElementById(`${this.c}${this.arr[r1]}`);
-    let p2 = document.getElementById(`${this.c}${this.arr[r2]}`);
 
-    p1.style.background = p2.style.background = swapColor;
+  async swap(p1, p2) {
+    p1.div.style.background = p2.div.style.background = swapColor;
 
-    let ofset1 = p1.offsetLeft;
-    let ofset2 = p2.offsetLeft;
+    let ofset1 = p1.xpos;
+    let ofset2 = p2.xpos;
+    p1.xpos = ofset2;
+    p2.xpos = ofset1;
 
-    p1.style.left = ofset2 + "px";
-    p2.style.left = ofset1 + "px";
-
+    p1.div.style.left = `${ofset2}px`;
+    p2.div.style.left = `${ofset1}px`;
     await new Promise((r) => setTimeout(r, 300));
-    p1.style.background = p2.style.background = defColor;
-    this.swapScore.innerHTML=Number(this.swapScore.innerHTML)+1;
+
+    p1.div.style.background = p2.div.style.background = defColor;
+    this.swapScore.innerHTML = Number(this.swapScore.innerHTML) + 1;
   }
 
-  async check(r1, r2) {
-    let p1 = document.getElementById(`${this.c}${this.arr[r1]}`);
-    let p2 = document.getElementById(`${this.c}${this.arr[r2]}`);
-
-    p1.style.background = p2.style.background = checkColor;
-
+  async check(p1, p2) {
+    p1.div.style.background = p2.div.style.background = checkColor;
     await new Promise((r) => setTimeout(r, 300));
-    p1.style.background = p2.style.background = defColor;
-    this.checkScore.innerHTML= Number(this.checkScore.innerHTML)+1;
+
+    p1.div.style.background = p2.div.style.background = defColor;
+    this.checkScore.innerHTML = Number(this.checkScore.innerHTML) + 1;
   }
 
   async bubbleSort() {
     for (let i = 0; i < this.arr.length; i++) {
       for (let j = 0; j < this.arr.length - i - 1; j++) {
-        await this.check(j, j + 1);
-        if (this.arr[j] > this.arr[j + 1]) {
+        await this.check(this.arr[j], this.arr[j + 1]);
+        if (this.arr[j].no > this.arr[j + 1].no) {
           [this.arr[j], this.arr[j + 1]] = [this.arr[j + 1], this.arr[j]];
-          await this.swap(j, j + 1);
+          await this.swap(this.arr[j], this.arr[j + 1]);
         }
       }
     }
@@ -89,36 +93,37 @@ class Aswap {
     for (let i = 0; i < this.arr.length; i++) {
       let min = i;
       for (let j = i + 1; j < this.arr.length; j++) {
-        await this.check(min, j);
-        if (this.arr[min] > this.arr[j]) {
+        await this.check(this.arr[min], this.arr[j]);
+        if (this.arr[min].no > this.arr[j].no) {
           min = j;
         }
       }
       if (i != min) {
         [this.arr[i], this.arr[min]] = [this.arr[min], this.arr[i]];
-        await this.swap(i, min);
+        await this.swap(this.arr[i], this.arr[min]);
       }
     }
   }
 
   async insertionSort() {
-    let j,s=0;
+    let j,
+      s = 0;
     for (let i = 1; i < this.arr.length; i++) {
       j = i - 1;
-      if(s==0){
-        await this.check(j, j + 1);
-        s=1;
+      if (s == 0) {
+        await this.check(this.arr[j], this.arr[j + 1]);
+        s = 1;
       }
-      while (j >= 0 && this.arr[j] > this.arr[j + 1]) {
-        if(s==0){
-          await this.check(j, j + 1);
+      while (j >= 0 && this.arr[j].no > this.arr[j + 1].no) {
+        if (s == 0) {
+          await this.check(this.arr[j], this.arr[j + 1]);
         }
         [this.arr[j], this.arr[j + 1]] = [this.arr[j + 1], this.arr[j]];
-        await this.swap(j, j + 1);
+        await this.swap(this.arr[j], this.arr[j + 1]);
         j -= 1;
-        s=0;
+        s = 0;
       }
-      s=0;
+      s = 0;
     }
   }
 }
@@ -126,24 +131,26 @@ class Aswap {
 function startSorting() {
   let sort1 = sorts1.value;
   let sort2 = sorts2.value;
-
-  if(sort1=='bubble'){
-    obja.bubbleSort();
-  }else if(sort1=='selection'){
-    obja.selectionSort();
-  }else if(sort1=='insertion'){
-    obja.insertionSort();
-  }else{
-    console.log('Invalid - a !!!')
+  switch (sort1) {
+    case "bubble":
+      obja.bubbleSort();
+      break;
+    case "selection":
+      obja.selectionSort();
+      break;
+    case "insertion":
+      obja.insertionSort();
+      break;
   }
-
-  if(sort2=='bubble'){
-    objb.bubbleSort();
-  }else if(sort2=='selection'){
-    objb.selectionSort();
-  }else if(sort2=='insertion'){
-    objb.insertionSort();
-  }else{
-    console.log('Invalid - b !!!')
+  switch (sort2) {
+    case "bubble":
+      objb.bubbleSort();
+      break;
+    case "selection":
+      objb.selectionSort();
+      break;
+    case "insertion":
+      objb.insertionSort();
+      break;
   }
 }
